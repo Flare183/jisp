023513d9348fd84e8fe6f3c013f55933d3e4c0b8
Jesse Richardson <richardson183@bellsouth.net>
2010-06-09 18:37:22 -0400

    Worked on the main menu

--- main.el
+++ main.el
@@ -1,2 +1,14 @@
-(print "Welcome to Jesse's Formula's Program")
-(print "Main Menu:")
+(print "Welcome to Jesse's Formula's Program
+Main Menu:
+Volume = 1
+Cylinder = 2
+SaCone = 3
+SaCylinder = 4
+VolCone = 5
+VolPyrad = 6
+VolSphre = 7
+SaCone = 8
+SaSphre = 9
+Circum = 10
+AreaCircle = 11
+")
